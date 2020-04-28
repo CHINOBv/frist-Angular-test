29bb4783cf589c63e4bbb9fdaacfecf0c6e1cb64
chinobv <retrochanel97@gmail.con>
2020-04-28 16:59:11 -0500

    Aprendi a usar NG For es como el .map que se usa en ReactJS

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Producto{
+  name: String,
+  price: Number,
+  stock: Number
+}
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
   styleUrls: ['./directiva.component.scss']
 })
+
+
 export class DirectivaComponent implements OnInit {
 
   loading:Boolean = true;
@@ -20,5 +27,23 @@ export class DirectivaComponent implements OnInit {
   showLoading(){
     this.loading = !this.loading;
   }
+
+  productos:Array<Producto> = [
+    {
+      name: "Coca",
+      price: 28,
+      stock: 20
+    },
+    {
+      name:"Vodka",
+      price: 98,
+      stock: 25
+    },
+    {
+      name: "Sprite",
+      price: 29,
+      stock: 30
+    }
+  ];
 
 }
